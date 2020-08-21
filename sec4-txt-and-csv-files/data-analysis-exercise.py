99f758d0270e2ff0bb419229ce9ebdc3263a5449
sumanth82 <sumanth82@gmail.com>
2020-08-21 07:50:00 -0400

    Updates

---
+++ sec4-txt-and-csv-files/data-analysis-exercise.py
@@ -0,0 +1,9 @@
+# Description: There’s an Excel file attached in this lecture. Please download it from the Resource section
+# As you see, the Excel file contains two columns. The column named GEOID contains the unique identifier for each of the 50 US states, while the 
+# 1984 column has the household median income for each state for year 1984.
+
+#Questions:
+
+#Open the Excel file in Python as a pandas dataframe.
+#Export the dataframe to a CSV file. Note that the CSV should not contain the first three header lines.
+
